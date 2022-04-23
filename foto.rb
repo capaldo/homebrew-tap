04819b4eec8b98bf7f9d4278fd41677bab67671b
capaldo <alex@capaldo.io>
2022-04-23 15:23:29 -0400

    Add additional details to the formula and update the version

--- foto.rb
+++ foto.rb
@@ -1,9 +1,11 @@
 class Foto < Formula
   desc "A command-line tool to copy and paste images in a variety of formats"
   homepage "https://github.com/capaldo/foto"
-  url "https://github.com/capaldo/foto/archive/refs/tags/v0.2.tar.gz"
-  sha256 "01a2169d0572ca9885037720b0ffe9b80e970f393c76e9536684f9d0009b3b8a"
-  
+  url "https://github.com/capaldo/foto/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "2a18db75ed6ff8960d97c76d68006c135e212e13c0a615bf0956b2758d888be6"
+  license "MIT"
+  head "https://github.com/capaldo/foto.git", branch: "main"
+
   def install
     system "swift", "build",
         "--configuration", "release",
